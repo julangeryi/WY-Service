6b4f3be6ced4efac6dca31f230a87ebaf0bf607b
julangeryi <julangeryi@sina.com>
2016-04-04 08:34:22 +0800

    修正了logincontroller

--- src/main/java/com/tgwy/controller/LoginController.java
+++ src/main/java/com/tgwy/controller/LoginController.java
@@ -1,32 +1,15 @@
 package com.tgwy.controller;
 
-import java.util.List;
-
-import javax.inject.Inject;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.tgwy.entity.User;
-import com.tgwy.service.UserService;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-	@Inject
-	private UserService userService;
-	@RequestMapping("index")
-	public String index(){
-		List<User> users = userService.listAll(new User());
-		for(User u : users){
-			System.out.println(u);
-		}
-		return "index";
-	}
-	
-	@RequestMapping("login")
-	public String login(){
-		
-		return "login";
+	@RequestMapping("/login")
+	public String main(@RequestParam(value="loginUserName")String loginUserName,@RequestParam(value="loginUserPass")String loginUserPass) {
+		System.out.println(loginUserPass);
+		return "main";
 	}
 }
