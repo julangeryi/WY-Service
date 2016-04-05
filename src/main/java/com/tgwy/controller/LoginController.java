da38c482b1d72a79d89f594b360b32c85ea21f11
julangeryi <julangeryi@sina.com>
2016-04-05 17:23:04 +0800

    第四次提交

--- src/main/java/com/tgwy/controller/LoginController.java
+++ src/main/java/com/tgwy/controller/LoginController.java
@@ -4,17 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.tg.wy.MybatisUtils;
-import com.tgwy.entity.User;
 
 @Controller
 public class LoginController {
 
 	@RequestMapping("/login")
 	public String main(@RequestParam(value="loginUserName")String loginUserName,@RequestParam(value="loginUserPass")String loginUserPass) {
-		User user = MybatisUtils.getUser();
 		
-		System.out.println(user.getNickname()+"---"+user.getPassword());
 		System.out.println(loginUserName+"---"+loginUserPass);
 		return "main";
 	}
